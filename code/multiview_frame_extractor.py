fcd9803c70d7dc551443b232732ea36d5f36f2f6
Sofia Broomé <sbroome@kth.se>
2020-02-13 15:33:04 +0100

    sketch for find video given time

--- code/multiview_frame_extractor.py
+++ code/multiview_frame_extractor.py
@@ -39,7 +39,7 @@ class MultiViewFrameExtractor():
         return subject_dir_path + start + '_' + end + '/'
 
     def get_view_dir_path(self, interval_dir_path, view):
-        return interval_dir_path + view + '/'
+        return interval_dir_path + str(view) + '/'
 
     def extract_frames(self):
         for i, subject in enumerate(self.subjects):
@@ -54,7 +54,9 @@ class MultiViewFrameExtractor():
                 # Just for directory naming
                 start_str = str(row['Start'])
                 end_str = str(row['End'])
-                interval_dir_path = self.get_interval_dir_path(subject_dir_path, start_str, end_str)
+                interval_dir_path = self.get_interval_dir_path(subject_dir_path,
+                                                               start_str,
+                                                               end_str)
 
                 # Timestamps for start and end
                 start = pd.to_datetime(row['Start'], format='%Y%m%d%H%M%S')
@@ -63,6 +65,8 @@ class MultiViewFrameExtractor():
 
                 for view in self.views:
                     view_dir_path = self.get_view_dir_path(interval_dir_path, view)
+
+                    video = self.find_video(subject, view, start)
 
                     # Identify the video containing the start
                     # Check if it also contains the end
@@ -104,6 +108,40 @@ class MultiViewFrameExtractor():
                     view_dir_path = self.get_view_dir_path(interval_dir_path, view)
                     subprocess.call(['mkdir', view_dir_path])
 
+    def find_video(self, subject, view, time):
+        """
+        subject: str (e.g. Aslan)
+        view: int (e.g. 0, use lookup table)
+        time: pd.datetime
+        returns video path str
+        """
+        subject_path = self.data_path + subject + '/'
+        lookup_viewpoint = pd.read_csv('../data/viewpoints.csv', index_col='Subject')
+        camera = lookup_viewpoint.at[subject, str(view)]
+        camera_str = 'ch0' + str(camera)
+
+        
+        date_dir = [dd for dd in os.listdir(subject_path)
+                    if pd.to_datetime(dd.split('/')[-1]).date() == time.date()]
+        date_path = subject_path + date_dir[0] + '/'
+        
+        camera_filenames = [fn[:fn.rindex('.')] for fn in os.listdir(date_path)
+                            if fn.startswith(camera_str) and '.mp4' in fn]
+
+        filename_times = [fn.split('_')[-1] for fn in camera_filenames]
+
+        time_stamps = map(from_filename_time_to_pd_datetime, filename_times)
+
+        # video_filename = 
+        import pdb; pdb.set_trace()
+
+        for path, dirs, files in sorted(os.walk(subject_path)):
+            date = pd.to_datetime(path.split('/')[-1]).date()
+            print(path)
+
+        return 'subj_path/date/video_path.mp4'
+        
+
 
 
 def check_if_unique_in_df(file_name, df):
@@ -120,4 +158,6 @@ def get_video_path(row):
     path = row['Subject'] + '/' + p + '/' + row['Video_ID'] + '.mp4'
     return path
 
+def from_filename_time_to_pd_datetime(yyyymmddHHMMSS):
+    return pd.to_datetime(yyyymmddHHMMSS, format='%Y%m%d%H%M%S')
 
