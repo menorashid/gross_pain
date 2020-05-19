7b24b18544943cc018529706ea620dc750546aa2
menoRashid <mhnrashid@gmail.com>
2020-05-19 13:25:37 -0700

    globals for calib board. not fully tested

--- code/script_calibrating_cameras.py
+++ code/script_calibrating_cameras.py
@@ -13,10 +13,16 @@ import itertools
 import matplotlib.pyplot as plt
 import shutil
 
+check_rows = 11
+# 11
+check_cols = 7
+h = 1520
+w = 2688
+
 def extract_calibration_vid_frames():
-    data_path = '../data/camera_calibration_videos'
-    out_dir = '../data/camera_calibration_frames_redo'
-    data_selection_path = '../metadata/interval_calibration.csv'
+    data_path = '../data/camera_calibration_videos_try2'
+    out_dir = '../data/camera_calibration_frames_try2'
+    data_selection_path = '../metadata/interval_calibration_try2.csv'
     util.mkdir(out_dir)
 
     mve = MultiViewFrameExtractor(data_path = data_path, width= 2688, height = 1520, frame_rate = 1., output_dir = out_dir,
@@ -27,19 +33,23 @@ def extract_calibration_vid_frames():
  #                 views = [3], data_selection_path = data_selection_path, num_processes = multiprocessing.cpu_count())
     # mve.extract_frames(replace = True, subjects_to_extract=['cell2'])
     
-    dirs_to_check = [os.path.join(out_dir, 'cell1','20200317130703_131800'), os.path.join(out_dir,'cell2','20200317130703_131800')]
+    # dirs_to_check = [os.path.join(out_dir, 'cell1','20200317130703_131800'), os.path.join(out_dir,'cell2','20200317130703_131800')]
     view_multiple_dirs(dirs_to_check, out_dir)
 
 def fix_filenames():
-    vid_dir = '../data/camera_calibration_videos/original_videos/2020-03-17'
     out_dir = '../data/camera_calibration_videos/cell1/2020-03-17'
+    vid_dir = '../data/camera_calibration_videos/original_videos/2020-03-17'
+    
+    out_dir = '../data/camera_calibration_videos_try2/cell1/2020-04-28'
+    vid_dir = '../data/camera_calibration_videos_try2/original_videos/2020-04-28'
+    
     util.makedirs(out_dir)
 
     # get video names
     vid_files = glob.glob(os.path.join(vid_dir,'*.mp4'))
     vid_files.sort()
 
-    # # extract first frame
+    # extract first frame
     # for vid_file in vid_files:
     #   # command = ['ffmpeg', '-i', vid_file, '-vf', '"select=eq(n\,0)"', '-q:v', '3', vid_file[:vid_file.rindex('.')]+'.jpg']
     #   command = ['ffmpeg', '-i', vid_file, '-y', '-vframes', '1', '-f', 'image2', vid_file[:vid_file.rindex('.')]+'.jpg']
@@ -48,8 +58,18 @@ def fix_filenames():
     # # view first frames
     # visualize.writeHTMLForFolder(vid_dir, height = 506, width = 896)
 
-    # # what're the times i see
-    new_times = ['130659', '130657', '130659', '130659', '131240', '130659', '130659','131150', '130659', '130659','131010']
+    # # # what're the times i see
+    # for vid_file in vid_files:
+    #     print (vid_file)
+    # new_times = ['130659', '130657', '130659', '130659', '131240', '130659', '130659','131150', '130659', '130659','131010']
+    new_times = ['104446','105105','111923',
+                '104441','110408',
+                '104446','105728','112545',
+                '104446','111210',
+                '104446','110735','113552',
+                '104446','110440','113255',
+                '104446','110203','113020',
+                '104446','105052','111908']
     
     # rename files
     for vid_file, new_time in zip(vid_files, new_times):
@@ -62,23 +82,27 @@ def fix_filenames():
 
 #  goes through a list of images and returns the ones with visible checkboard
 def get_img_chessboard(fname):
-    check_cols = 7
-    check_rows = 9
+    # check_cols = 6
+    # check_rows = 9
+    # check_rows = 10
+    global check_rows, check_cols
+    print (check_rows, check_cols)
     img = cv2.imread(fname)
     gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
     # detect chessboard
     ret, corners = cv2.findChessboardCorners(gray, (check_cols,check_rows),cv2.CALIB_CB_FAST_CHECK)
-    
+    print (ret)
     if ret == True:
+        
+        out_file = '../scratch/check_cell2.jpg'
+        dst = cv2.drawChessboardCorners(img, (check_cols,check_rows), corners,ret)
+        cv2.imwrite(out_file,dst)
         return fname
         
 
 def do_intrinsic(im_list, out_dir = False):
-    check_cols = 7
-    check_rows = 9
-    h = 1520
-    w = 2688
-
+    global check_rows, check_cols, h, w
+    
     # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
     objp = np.zeros((check_cols*check_rows,3), np.float32)
     objp[:,:2] = np.mgrid[0:check_cols,0:check_rows].T.reshape(-1,2)
@@ -89,13 +113,14 @@ def do_intrinsic(im_list, out_dir = False):
 
     # load list of images
     for idx_fname, fname in enumerate(im_list):
+        print (idx_fname)
         img = cv2.imread(fname)
         
         gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
         
         # detect chessboard
         ret, corners = cv2.findChessboardCorners(gray, (check_cols,check_rows),None)
-        
+        assert ret 
         if ret == True:
             objpoints.append(objp)
 
@@ -132,6 +157,10 @@ def get_all_files_with_chessboard():
 
     meta_dir = '../data/camera_calibration_frames_redo'
     interval_str = '20200317130703_131800'
+
+    meta_dir = '../data/camera_calibration_frames_try2'
+    interval_str = '20200428104445_113700'
+
     out_dir = os.path.join(meta_dir, 'ims_to_keep')
     util.mkdir(out_dir)
     
@@ -142,13 +171,13 @@ def get_all_files_with_chessboard():
             dir_curr = os.path.join(meta_dir,cell_str,interval_str,str(view))
             im_list = glob.glob(os.path.join(dir_curr,'*.jpg'))
             im_list.sort()
+            lim = 1650
             if idx_cell==0:
-                im_list = im_list[:len(im_list)//2]
+                im_list = im_list[:lim]
             else:
                 # no one's in cell 2 till second half of videos
-                im_list = im_list[len(im_list)//2:]
+                im_list = im_list[lim:]
             print (len(im_list))
-            
             pool = multiprocessing.Pool(multiprocessing.cpu_count())
             ret_vals = pool.map(get_img_chessboard,im_list)
             ret_vals = [val for val in ret_vals if val is not None]
@@ -222,6 +251,10 @@ def rough_script_intrinsic():
 def visualize_all_files_with_chessboard():
     meta_dir = '../data/camera_calibration_frames_redo'
     interval_str = '20200317130703_131800'
+
+    meta_dir = '../data/camera_calibration_frames_try2'
+    interval_str = '20200428104445_113700'
+
     out_dir = os.path.join(meta_dir, 'ims_to_keep')
     
 
@@ -229,20 +262,26 @@ def visualize_all_files_with_chessboard():
     str_replace = ['..','/gross_pain']
     util.mkdir(out_dir_html)
 
-    common_im_dict = get_common_im(out_dir)
+    common_im_dict = get_common_im(out_dir,['cell2'])
+        # ,1,2,3])
     for key_curr in common_im_dict.keys():
 
         im_cols = common_im_dict[key_curr]
-        print (key_curr, len(im_cols))
+        # im_cols = [im_col[-300:-200] for im_col in im_cols]
+        print (key_curr, len(im_cols),len(im_cols[0]))
         # im_cols = np.array(im_cols)
         if len(im_cols[0])>0:
             out_file_html = os.path.join(out_dir_html,'_'.join([str(val) for val in key_curr])+'.html')
-
+            captions =[]
             for idx_im_col in range(len(im_cols)):
+                captions.append([])
                 for idx_im in range(len(im_cols[idx_im_col])):
+                    # print (im_cols[idx_im_col][idx_im])
+                    # s = input()
                     im_cols[idx_im_col][idx_im] = im_cols[idx_im_col][idx_im].replace(str_replace[0],str_replace[1])
+                    captions[idx_im_col].append(os.path.split(im_cols[idx_im_col][idx_im])[1])
             views = list(key_curr)[1:]
-            captions = [[str(view)]*len(im_cols[idx_view]) for idx_view, view in enumerate(views)]
+            # captions = [[str(view)]*len(im_cols[idx_view]) for idx_view, view in enumerate(views)]
             visualize.writeHTML(out_file_html, im_cols, captions)
 
 def save_common_im_with_chessboard_det():
@@ -271,8 +310,8 @@ def save_common_im_with_chessboard_det():
                 util.makedirs(os.path.split(out_file)[0])
                 
 
-                check_cols = 7
-                check_rows = 9
+                # check_cols = 7
+                # check_rows = 9
                 img = cv2.imread(im_file)
                 gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
                 # detect chessboard
@@ -293,11 +332,13 @@ def save_common_im_with_chessboard_det():
 
 def script_save_all_intrinsics():
     meta_dir = '../data/camera_calibration_frames_redo'
-    out_dir_chess_det = '../data/camera_calibration_frames_withChessboardDet'
-    util.mkdir(out_dir_chess_det)
     interval_str = '20200317130703_131800'
+
+    meta_dir = '../data/camera_calibration_frames_try2'
+    interval_str = '20200428104445_113700'
+
     out_dir = os.path.join(meta_dir, 'ims_to_keep')
-    out_dir_intrinsic = os.path.join(meta_dir,'intrinsics')
+    out_dir_intrinsic = os.path.join(meta_dir,'intrinsics_10')
     util.mkdir(out_dir_intrinsic)
 
     common_im_dict = get_common_im(out_dir)
@@ -307,7 +348,7 @@ def script_save_all_intrinsics():
 
         im_files = common_im_dict[key_curr]
         assert len(im_files)==1
-        im_files = im_files[0]
+        im_files = im_files[0][::10]
         # [:10]
         print (key_curr, len(im_files))
         # util.mkdir('../scratch/check_row_col')
@@ -324,8 +365,8 @@ def script_save_all_intrinsics():
 def save_im_chessboard_dets(arg):
     (im_file, out_file, out_file_viz) = arg
     num_dets = 4 
-    check_cols = 7 
-    check_rows = 9
+    # check_cols = 7 
+    # check_rows = 9
     try:
         print (im_file)
         img = cv2.imread(im_file)
@@ -493,10 +534,10 @@ def script_stereo_calibrate():
     out_dir = os.path.join(meta_dir, 'ims_to_keep')
     out_dir_intrinsic = os.path.join(meta_dir, 'intrinsics')
     out_dir_dets = os.path.join(meta_dir, 'chessboard_dets')
-    check_cols = 7
-    check_rows = 9
-    h = 1520
-    w = 2688
+    # check_cols = 7
+    # check_rows = 9
+    # h = 1520
+    # w = 2688
 
     cell_num = '1'
     views = ['0','3']
@@ -743,8 +784,8 @@ def script_calibrate_center_board():
     out_dir = '../scratch/center_box'
     util.mkdir(out_dir)
 
-    check_rows = 7
-    check_cols = 9
+    # check_rows = 7
+    # check_cols = 9
     objp = np.zeros((check_cols*check_rows,3), np.float32)
     objp[:,:2] = np.mgrid[0:check_cols,0:check_rows].T.reshape(-1,2)
     print (objp[:20])
@@ -828,16 +869,23 @@ def main():
     # extract_calibration_vid_frames()
     # get_all_files_with_chessboard()
     # visualize_all_files_with_chessboard()
+    script_save_all_intrinsics()
     # save_common_im_with_chessboard_det()
 
     # script_stereo_calibrate()
 
     # script_calibrate_manual()
 
-    script_calibrate_center_board()
+    # script_calibrate_center_board()
 
 
 
+    
+    # fname = '../data/camera_calibration_frames_try2/cell1/20200428104445_113700/0/ce_00_0_000998.jpg'
+    # fname = '../data/camera_calibration_frames_try2/cell2/20200428104445_113700/0/ce_00_0_002897.jpg'
+    # http://vision8.idav.ucdavis.edu:8000/gross_pain/data/camera_calibration_frames_try2/cell2/20200428104445_113700/0/ce_00_0_002897.jpg
+    # print ()
+    # get_img_chessboard(fname)
     return
     meta_dir = '../data/camera_calibration_frames_redo'
     interval_str = '20200317130703_131800'
